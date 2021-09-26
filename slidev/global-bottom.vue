492d081033feca66dd452349b1699d9815eac4b9
Fanurs <fyanurs@gmail.com>
2021-09-26 00:25:05 -0400

    :wrench: Added custom scripts

---
+++ slidev/global-bottom.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer
+    v-if="$slidev.nav.currentLayout !== 'cover'"
+    class="absolute top-0 right-0 p-0.5 text-xs bg-lime-300 opacity-50 rounded"
+  >
+    @Fanurs {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+  </footer>
+</template>
---
+++ slidev/set-project.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -f slides.md
+ln -s $1/slides.md slides.md
