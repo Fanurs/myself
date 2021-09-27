e549145c4c4e1c485151fbb5589ea5d7f62e6ad8
Fanurs <fyanurs@gmail.com>
2021-09-27 18:33:26 -0400

    :lipstick: Make slide number design simpler

--- slidev/global-bottom.vue
+++ slidev/global-bottom.vue
@@ -1,7 +1,7 @@
 <template>
   <footer
     v-if="$slidev.nav.currentLayout !== 'cover'"
-    class="absolute top-0 right-0 p-0.5 text-xs bg-lime-300 opacity-50 rounded"
+    class="absolute top-0 right-0 p-0.5 text-xs"
   >
     @Fanurs {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
   </footer>
