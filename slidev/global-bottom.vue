644f2ce44f276309b0f7e6126a7cd983c423caf6
Fanurs <fyanurs@gmail.com>
2021-09-27 23:28:20 -0400

    :children_crossing: Added link to gh homepage

--- slidev/global-bottom.vue
+++ slidev/global-bottom.vue
@@ -3,6 +3,6 @@
     v-if="$slidev.nav.currentLayout !== 'cover'"
     class="absolute top-0 right-0 p-0.5 text-xs"
   >
-    @Fanurs {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+    <a href="https://fanurs.github.io/myself/" title="Personal homepage">@Fanurs</a> {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
   </footer>
 </template>
